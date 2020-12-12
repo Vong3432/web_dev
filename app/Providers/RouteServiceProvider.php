c7866e1f08d82636ddffe9d921e3d1a00904e235
Vong3432 <vongnyuksoon2000@gmail.com>
2020-12-12 17:13:12 +0800

    Fix https

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -47,6 +47,11 @@ class RouteServiceProvider extends ServiceProvider
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
+
+        if(config('app.env') === 'production') {
+            resolve(\Illuminate\Routing\UrlGenerator::class)->forceScheme('https');
+            parent::boot();
+        }            
     }
 
     /**
